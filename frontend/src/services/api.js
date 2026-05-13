3798205c2520c652cb74113ef147d1f12b21692a
Rakshitha <rakshitha0263@gmail.com>
2026-05-13 13:49:51 +0530

    Connect frontend to Railway backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://team-task-manager-production-e863.up.railway.app/api",
 });
 
 API.interceptors.request.use((req) => {
